d82a1c3a0adff6b9d5d86da8ee7579d845f87a33
keyboardreamer <104249142+keyboardreamer@users.noreply.github.com>
2022-09-10 20:22:12 +0800

    Add files via upload

---
+++ create.js
@@ -0,0 +1,7 @@
+function done(){
+    document.querySelector('.bar').innerHTML = `
+        <img src='loadingGuy.png'>
+        <h2>We are verifying your request :)</h2>
+        <p>this step is to make sure that the fundraiser is not a scam</p>
+    `
+}
