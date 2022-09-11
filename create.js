51a55ac9645f9b3dffa4c0191a03a5e8a2f3022e
keyboardreamer <104249142+keyboardreamer@users.noreply.github.com>
2022-09-11 09:22:35 +0800

    Update create.js

--- create.js
+++ create.js
@@ -4,4 +4,7 @@ function done(){
         <h2>We are verifying your request :)</h2>
         <p>this step is to make sure that the fundraiser is not a scam</p>
     `
+      setInterval(()=>{
+        window.location.replace('https://keyboardreamer.github.io/wefundlove/index.html');
+    },3000)
 }
